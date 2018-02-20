ab406a4beb511fe874c6783fce1fa6981af15a8d
Marcos-Frias <marcos.frias.martinez@hotmail.com>
2018-02-20 15:51:00 -0600

    general controller

--- controllers/controller.php
+++ controllers/controller.php
@@ -1,29 +1,51 @@
 
 <?php
-		/**
-		 *
-		 
-		class Controller{
 
-			public function inicio(){
-				include "Views/user-login.php";
-			}
+class controller {
 
-			#REGISTRO DE USUARIOS
-			public function loginUserController(){
-				$datosCon = array('nickname' =>$_POST["nickname"], "password" => $_POST["password"]);
-				$respuesta = Datos::loginUserModel($datosCon, "");
-			}
+	private $modelGeneral;
 
+	function __construct(){
+		/*$this->modelGeneral = new modeloGeneral();*/
+	}
 
-			#REGISTRO DE ALUMNOS
-			public function creatUserController(){
+	public function isSesion() {
+		return (isset($_SESSION["doctor"]) );
+	}
 
-				$datosCon = array('nombre'=>$_POST["nameAl"],'aPaterno'=>$_POST["aPatAl"],'aMaterno'=>$_POST["aMatAl"],'phone'=>$_POST["phoneAl"],'grado'=>$_POST["gradoAl"] ,'grupo'=>$_POST["grupoAl"]);
-				$respuesta = Datos::creatUserModel($datosCon,"usuario");
-			}
-
+	public function auntentificacion() {
+		if (isset($_POST["usuario"]) && isset($_POST["password"])) {
+				return $this->validarAcceso($_POST["usuario"], $_POST["password"]);
 		}
-*/
+		return "";
+	}
+
+	public function validarAcceso($username, $password) {
+		$username = filter_var($username, FILTER_SANITIZE_STRING);
+		$password = filter_var($password, FILTER_SANITIZE_STRING);
+		$user = $this->modeloDoc->consultarUsuario($username, $password);
+		if (!$user) {
+				return $this->mensaje("Error de contraseña y/o privilegios ");
+		} else {
+						$_SESSION["doctor"] = $doctor[0];
+		}
+		return $this->mensaje("Inicio correcto", 1);
+	}
+
+	public function redireccion($url) {
+		header("Location: " . $url);
+	}
+
+	public function mensaje($mensaje, $type = 0) {
+		if ($type == 0) {
+			return '<div class="alert alert-danger"><i class="fa fa-times-circle fa-fw fa-lg"></i><b>' . $mensaje . '</b></div>';
+		}
+		return '<div class="alert alert-success"><i class="fa fa-check-circle fa-fw fa-lg"></i><b>' . $mensaje . '</b></div>';
+	}
+
+	public function inicio(){
+		include "Views/user-login.php";
+	}
+}
 
  ?>
